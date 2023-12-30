cce45c34a9795f3d6d63a5d41e76d06cc873dcd8
olena099 <142909583+olena099@users.noreply.github.com>
2023-12-30 07:16:24 -0800

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,29 @@
+    }
+}
+function generateRandomData() {
+
+    data.forEach(item => {
+function main() {
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+
+    const data = [];
+    const data = generateRandomData();
+    });
+main();
+}    main()
+import random
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def main():
+def generate_random_data():
+if __name__ == "__main__":
+
+    for item in data:
